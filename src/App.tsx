de9c316181f00b28996c575af1252697fc8a6fd6
stratis <stratis@users.noreply.github.com>
2023-06-21 01:48:11 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+import PositionTitle from "./PositionTitle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PositionTitle
+        underlying="ABNB"
+        positionType="Iron Condor"
+        expiry="43d"
+        isOpen="True"
+      />
+    </div>
+  );
+}
---
+++ src/PositionTitle.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Card, Typography, makeStyles } from "@material-ui/core";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
+const useStyles = makeStyles({
+  card: {
+    padding: "16px",
+    margin: "16px 0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#cedaf5"
+  },
+  positionType: {
+    color: "gray",
+    marginLeft: "8px"
+  },
+  underlying: {
+    fontWeight: "bold"
+  },
+  expiry: {
+    color: "green",
+    marginLeft: "8px"
+  },
+  separator: {
+    margin: "0 8px"
+  },
+  open: {
+    color: "green"
+  },
+  closed: {
+    color: "red"
+  }
+});
+
+const PositionCard = ({ positionType, underlying, expiry, isOpen }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <FiberManualRecordIcon
+        className={isOpen ? classes.open : classes.closed}
+      />
+      <Typography variant="h6" className={classes.underlying}>
+        {underlying}
+      </Typography>
+      <Typography variant="body2" className={classes.separator}>
+        |
+      </Typography>
+      <Typography variant="subtitle1" className={classes.positionType}>
+        {positionType}
+      </Typography>
+      <Typography variant="body2" className={classes.separator}>
+        |
+      </Typography>
+      <Typography variant="body1" className={classes.expiry}>
+        {expiry}
+      </Typography>
+    </Card>
+  );
+};
+
+export default PositionCard;
